4f41ae7cc3a22e5b264a2edcc75222b7ee8562ee
dang2411 <ghostvshell@gmail.com>
2022-12-02 16:30:55 +0700

    up

--- src/sections/@dashboard/request/card/RequestNewEditImageCard.tsx
+++ src/sections/@dashboard/request/card/RequestNewEditImageCard.tsx
@@ -2,8 +2,8 @@ import { Box } from '@mui/material';
 
 export default function RequestNewEditImageCard({ image, onClick, ...rest }: any) {
   return (
-    <Box onClick={onClick} display={"flex"} justifyContent = "center" alignItems={"center"} >
-      <Box component="img" src={image} alt="image" {...rest} maxHeight="250px" />
+    <Box mt={0} onClick={onClick} display={'flex'} justifyContent="center">
+      <Box component="img" src={image} alt="image" {...rest} maxHeight="200px" />
     </Box>
   );
 }
